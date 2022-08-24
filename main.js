4cb546637d3f7198cd2c02d3db6296162fceb1e7
julianbrodsky <brodskyjulian@yahoo.com>
2022-08-24 11:11:16 -0600

    working prototype

--- main.js
+++ main.js
@@ -59,60 +59,15 @@ class BasicWorldDemo {
 
     const loader = new THREE.CubeTextureLoader();
     const texture = loader.load([
-        './resources/posx.jpg',
-        './resources/negx.jpg',
-        './resources/posy.jpg',
-        './resources/negy.jpg',
-        './resources/posz.jpg',
-        './resources/negz.jpg',
+        './resources/posx.png',
+        './resources/negx.png',
+        './resources/posy.png',
+        './resources/negy.png',
+        './resources/posz.png',
+        './resources/negz.png',
     ]);
     this._scene.background = texture;
 
-    const plane = new THREE.Mesh(
-        new THREE.PlaneGeometry(100, 100, 10, 10),
-        new THREE.MeshStandardMaterial({
-            color: 0xFF4BEA,
-          }));
-    plane.castShadow = false;
-    plane.receiveShadow = true;
-    plane.rotation.x = -Math.PI / 2;
-    this._scene.add(plane);
-
-    const box = new THREE.Mesh(
-      new THREE.BoxGeometry(2, 2, 2),
-      new THREE.MeshStandardMaterial({
-          color: 0xFFFFFF,
-      }));
-    box.position.set(0, 1, 0);
-    box.castShadow = true;
-    box.receiveShadow = true;
-    this._scene.add(box);
-
-    // for (let x = -8; x < 8; x++) {
-    //   for (let y = -8; y < 8; y++) {
-    //     const box = new THREE.Mesh(
-    //       new THREE.BoxGeometry(2, 2, 2),
-    //       new THREE.MeshStandardMaterial({
-    //           color: 0x808080,
-    //       }));
-    //     box.position.set(Math.random() + x * 5, Math.random() * 4.0 + 2.0, Math.random() + y * 5);
-    //     box.castShadow = true;
-    //     box.receiveShadow = true;
-    //     this._scene.add(box);
-    //   }
-    // }
-
-    // const box = new THREE.Mesh(
-    //   new THREE.SphereGeometry(2, 32, 32),
-    //   new THREE.MeshStandardMaterial({
-    //       color: 0xFFFFFF,
-    //       wireframe: true,
-    //       wireframeLinewidth: 4,
-    //   }));
-    // box.position.set(0, 0, 0);
-    // box.castShadow = true;
-    // box.receiveShadow = true;
-    // this._scene.add(box);
 
     this._RAF();
   }
